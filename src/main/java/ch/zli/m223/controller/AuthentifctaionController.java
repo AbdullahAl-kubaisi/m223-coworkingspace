720ce36cfcd5627d0adb7aa764cdf92818ce36ab
Al-Kubaisi Abdullah <Abdullah.Al-Kubaisi@swisscom.com>
2022-11-23 08:39:29 +0100

    created authentifcation prosses

---
+++ src/main/java/ch/zli/m223/controller/AuthentifctaionController.java
@@ -0,0 +1,57 @@
+
+package ch.zli.m223.controller;
+
+import ch.zli.m223.model.User;
+import ch.zli.m223.service.AuthentifctaionService;
+import ch.zli.m223.service.UserService;
+
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/auth")
+
+@Tag(name = "Authentifizierung", description = "Authentifizierung")
+public class AuthentifctaionController {
+    
+        @Inject
+        AuthentifctaionService authentifctaionService;
+    
+        @Inject
+        UserService userService;
+
+        @Inject
+        JsonWebToken jwt; 
+    
+        @POST
+        @Path("/login")
+        @PermitAll
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response login(@QueryParam("email")String email, @QueryParam("password") String password) {
+            return authentifctaionService.login(email, password);
+        }
+    
+        @POST
+        @Path("/register")
+        @PermitAll
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        public User register(User user) {
+            return userService.create(user);
+        }
+    
+        @POST
+        @Path("/logout")
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        public Response logout(String token) {
+            return Response.ok(authentifctaionService.logout(token)).build();
+        }
+
+}
