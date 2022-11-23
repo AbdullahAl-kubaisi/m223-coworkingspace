1f2055aff3fe27080cbaf1ea7b843507fdf08aa7
Al-Kubaisi Abdullah <Abdullah.Al-Kubaisi@swisscom.com>
2022-11-23 09:00:51 +0100

    created unit tests to test the login methods

--- src/test/java/ch/zli/m223/AuthentifcatiinRessourceTest.java
+++ src/test/java/ch/zli/m223/AuthentifcatiinRessourceTest.java
@@ -1,19 +1,17 @@
 package ch.zli.m223;
 
-import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
-import org.gradle.internal.impldep.javax.annotation.meta.When;
 
 @QuarkusTest
 public class AuthentifcatiinRessourceTest {
     
+
 
     @Test
     public void testPostLogin() {
@@ -56,5 +54,7 @@ public class AuthentifcatiinRessourceTest {
             .then()
             .statusCode(404);
     }
+
+
 
 }
